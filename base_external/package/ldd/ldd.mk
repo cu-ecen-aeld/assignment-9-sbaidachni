d3b1c400d654761326d36ea69aee153f78cf8481
sbaidachni <sbaidachni@gmail.com>
2024-08-13 21:37:56 -0700

    simplification

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -21,12 +21,6 @@ LDD_MODULE_SUBDIRS += scull/
 LDD_MODULE_SUBDIRS += misc-modules/
 
 define LDD_INSTALL_TARGET_CMDS
-
-	$(INSTALL) -d $(TARGET_DIR)/lib
-	$(INSTALL) -m  0644 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/
-	$(INSTALL) -m  0644 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/
-	$(INSTALL) -m  0644 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/
-
 	$(INSTALL) -m  0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m  0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m  0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
