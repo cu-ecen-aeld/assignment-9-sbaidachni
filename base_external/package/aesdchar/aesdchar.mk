4007746e7c19a95388eab506f1853f3f2767a584
sbaidachni <sbaidachni@gmail.com>
2024-08-14 20:10:09 -0700

    assignment 8 init code

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,29 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = 'fd5880d8199d55cd9cf2a676f739442e4a1f80c5'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-sbaidachni.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_LICENSE = GPL-2.0
+AESDCHAR_LICENSE_FILES = COPYING
+
+AESDCHAR_MODULE_SUBDIRS += aesd-char-driver/
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m  0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m  0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
