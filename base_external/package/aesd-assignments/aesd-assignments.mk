9e3dfed6c16b4957a85bce45f0194f5a395d9fe6
sbaidachni <sbaidachni@gmail.com>
2024-08-15 14:04:30 -0700

    test new read approach

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'fd640ff5b1f19312d0e67e02b104ac1a2832045d'
+AESD_ASSIGNMENTS_VERSION = '0ba32cad3a4791dff02b16b62a14af3fdb4445c5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
